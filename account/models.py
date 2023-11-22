8d54cacfd35fe6fd9562881eb4dacbd98999c9ea
Lee816 <lgha0816@naver.com>
2023-11-22 23:30:54 +0900

    account 앱 User 모델 작성

--- account/models.py
+++ account/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from datetime import datetime
+
+
+def user_image_path(instance, filename):
+    return f"account/{datetime.today().year}년{datetime.today().month}월/{datetime.today().day}/{instance.username}/{filename}"
+
+
+class User(AbstractUser):
+    # 사용 x
+    first_name = None
+    last_name = None
+
+    # 사용 o
+    username = models.CharField(max_length=25)
+    nickname = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    introduce = models.TextField(blank=True)
+    user_image = models.ImageField(default="user.png", upload_to=user_image_path)
+
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers", blank=True, null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # 로그인 id로 사용할 필드
+    USERNAME_FIELD = "email"
+    # 필수 작성 필드
+    REQUIRED_FIELDS = ["username", "phone"]
+
+    def __str__(self):
+        return self.username
