650aed6201a3ca827f30f44c00c4b19687ecdd2f
Lee816 <lgha0816@naver.com>
2023-11-22 23:31:39 +0900

    User 모델 admin 사이트 등록

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "nickname", "email", "phone", "created_at"]
+    list_filter = ["created_at"]
+    search_fields = ["username", "nickname", "email", "phone"]
+    ordering = ["-created_at"]
