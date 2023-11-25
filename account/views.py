28af841d749908c8430246660d3fd24c80426698
Lee816 <lgha0816@naver.com>
2023-11-25 13:03:03 +0900

    회원가입 뷰 작성

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import User
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "account/register.html")
+
+    def post(self, request):
+        username = request.POST["username"]
+        nickname = request.POST["nickname"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+
+        new_user = User.objects.create(
+            username=username, nickname=nickname, email=email, phone=phone
+        )
+        new_user.set_password(request.POST["password"])
+        new_user.save()
+
+        return HttpResponse("가입완료")
