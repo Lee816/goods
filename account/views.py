e0e45570eee894af46c045e7c590facf53f6c819
Lee816 <lgha0816@naver.com>
2023-11-27 14:18:51 +0900

    로그인 뷰 작성

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 from .models import User
 
@@ -22,3 +23,20 @@ class RegisterView(View):
         new_user.save()
 
         return HttpResponse("가입완료")
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "account/login.html")
+
+    def post(self, request):
+        email = request.POST["email"]
+        password = request.POST["password"]
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("account:login")
+
+        return render(request, "base/bad_request.html")
