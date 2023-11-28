616d40b8669c6c5f8b6d86cd806a27955e9fc763
Lee816 <lgha0816@naver.com>
2023-11-28 17:49:50 +0900

    goods list 뷰 수정

--- goods/views.py
+++ goods/views.py
@@ -5,10 +5,10 @@ from django.views import View
 from .models import Goods, Category, Entertainer, Design
 
 
-class GoodsView(LoginRequiredMixin, View):
+class GoodsListView(LoginRequiredMixin, View):
     def get(self, request):
         goods_list = Goods.objects.all()
         return render(request, "goods/goods_list.html", {"goods_list": goods_list})
 
     def post(self, request):
-        pass
+        return render(request, "base/bad_request.html")
