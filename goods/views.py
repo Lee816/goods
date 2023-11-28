859998d61f53bc21cbc53a5ee0836c03e4b8625b
Lee816 <lgha0816@naver.com>
2023-11-28 17:47:23 +0900

    goods 리스트 뷰 작성

--- goods/views.py
+++ goods/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
-# Create your views here.
+from .models import Goods, Category, Entertainer, Design
+
+
+class GoodsView(LoginRequiredMixin, View):
+    def get(self, request):
+        goods_list = Goods.objects.all()
+        return render(request, "goods/goods_list.html", {"goods_list": goods_list})
+
+    def post(self, request):
+        pass
