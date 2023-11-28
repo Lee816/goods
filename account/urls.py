ba1147f623313879ff136f1b678e04438135de20
Lee816 <lgha0816@naver.com>
2023-11-28 14:52:51 +0900

    비밀번호 초기화 및 변경 url 작성

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path("update/<int:pk>/", views.UpdateView.as_view(), name="update"),
+    path("pwupdate/<int:pk>/", views.PWUpdateView.as_view(), name="pw_update"),
+    path("pwreset/<int:pk>/", views.PWResetView.as_view(), name="pw_reset"),
+    path("pw_confirm/<int:pk>/", views.PWResetConfirmView.as_view(), name="pw_confirm"),
 ]
