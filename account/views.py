a199ada040602ca4a18c4ec6a8d61955fece65da
Lee816 <lgha0816@naver.com>
2023-11-28 14:52:42 +0900

    비밀번호 초기화 및 변경 뷰 작성

--- account/views.py
+++ account/views.py
@@ -70,3 +70,64 @@ class UpdateView(User_Update_Permission, View):
         user.save()
 
         return redirect("home")
+
+
+class PWUpdateView(User_Update_Permission, View):
+    def get(self, request, pk):
+        return render(request, "account/pw_update.html")
+
+    def post(self, request, pk):
+        password = request.POST["password"]
+        new_password1 = request.POST["new_password1"]
+        new_password2 = request.POST["new_password2"]
+
+        user = get_object_or_404(User, id=pk)
+
+        if (
+            user is not None
+            and password == user.password
+            and user == request.user
+            and new_password1 == new_password2
+        ):
+            user.set_password(new_password1)
+            user.save()
+            logout(request)
+            return redirect("account:login")
+
+        return render(request, "account/pw_update.html")
+
+
+class PWResetView(User_Update_Permission, View):
+    def get(self, request, pk):
+        return render(request, "account/pw_reset.html")
+
+    def post(self, request, pk):
+        email = request.POST["email"]
+        username = request.POST["username"]
+        phone = request.POST["phone"]
+
+        user = get_object_or_404(User, email=email, username=username, phone=phone)
+
+        if user is not None and user == request.user and user.id == pk:
+            return render(request, "account/pw_reset_confirm.html", {"user": user})
+
+        return render(request, "base/bad_request.html")
+
+
+class PWResetConfirmView(User_Update_Permission, View):
+    def get(self, request, pk):
+        return render(request, "base/bad_request.html")
+
+    def post(self, request, pk):
+        new_password1 = request.POST["new_password1"]
+        new_password2 = request.POST["new_password2"]
+
+        user = get_object_or_404(User, id=pk)
+
+        if user is not None and new_password1 == new_password2:
+            user.set_password(new_password1)
+            user.save()
+            logout(request)
+            return redirect("account:login")
+
+        return render(request, "account/pw_reset_confirm.html", {"user": user})
