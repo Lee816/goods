314115192c601fd7fcb1bfeaa8b8164073e06ab8
Lee816 <lgha0816@naver.com>
2023-11-28 18:44:03 +0900

    goods 디자인 추가 뷰 작성

--- goods/views.py
+++ goods/views.py
@@ -12,3 +12,34 @@ class GoodsListView(LoginRequiredMixin, View):
 
     def post(self, request):
         return render(request, "base/bad_request.html")
+
+
+class GoodsCreateView(LoginRequiredMixin, View):
+    def get(self, request):
+        category = Category.objects.all().order_by("name")
+        entertainer = Entertainer.objects.all().order_by("name")
+        return render(
+            request,
+            "goods/goods_create.html",
+            {"category": category, "entertainer": entertainer},
+        )
+
+    def post(self, request):
+        category = get_object_or_404(Category, id=request.POST["category"])
+        entertainer = get_object_or_404(Entertainer, id=request.POST["entertainer"])
+        description = request.POST["description"]
+        design_list = request.FILES.getlist("design")
+
+        goods = Goods.objects.create(
+            category=category,
+            entertainer=entertainer,
+            creator=request.user,
+            description=description,
+        )
+        goods.save()
+
+        for design in design_list:
+            new_design = Design.objects.create(goods=goods, design=design)
+            new_design.save()
+
+        return redirect("goods:goods_list")
