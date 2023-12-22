33d1a3ecc092488591548589d7196fe8b2f0d67d
Lee816 <lgha0816@naver.com>
2023-12-22 11:42:15 +0900

    웹소켓 통신을 위한 asgi.py 수정

--- config/asgi.py
+++ config/asgi.py
@@ -10,7 +10,27 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
-application = get_asgi_application()
+from chat.consumers import ChatComsumer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(
+                    [
+                        path("ws/<int:pk>/chatroom", ChatComsumer.as_asgi()),
+                    ]
+                )
+            )
+        ),
+    }
+)
