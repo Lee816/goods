17abc76e3eafc8b5fd13f7cc90569240780319e9
lee <lgha0816@naver.com>
2024-02-22 20:13:39 +0900

    채팅방 없을시 에러 출력말고 채팅방 생성하도록 변경경

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
@@ -17,15 +17,24 @@ class RoomListView(View):
 class RoomView(View):
     def get(self, request, pk):
         opponent = get_object_or_404(User, id=pk)
-        room = Room.objects.filter(
+
+        if Room.objects.filter(
             Q(user1=request.user, user2=opponent)
             | Q(user2=request.user, user1=opponent)
-        )
-        if len(room) == 1:
-            messages = Message.objects.filter(room=room[0])
+        ).exists():
 
-            return render(
-                request, "chat/room.html", {"room": room[0], "messages": messages}
+            room = Room.objects.filter(
+                Q(user1=request.user, user2=opponent)
+                | Q(user2=request.user, user1=opponent)
             )
+            if len(room) == 1:
+                messages = Message.objects.filter(room=room[0])
+
+                return render(
+                    request, "chat/room.html", {"room": room[0], "messages": messages}
+                )
+            else:
+                raise ValueError("Room Error")
         else:
-            raise ValueError("Room Error")
+            room = Room.objects.create(user1=request.user, user2=opponent)
+            return render(request, "chat/room.html", {"room": room})
